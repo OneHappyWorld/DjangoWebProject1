2666578485ebd6b5690fe5e6371f90126d33dead
OneHappyWorld <OneHappyWorld@users.noreply.github.com>
2019-01-14 19:14:12 -0600

    Upgrade Django

--- DjangoWebProject1/manage.py
+++ DjangoWebProject1/manage.py
@@ -6,12 +6,16 @@ Command-line utility for administrative tasks.
 import os
 import sys
 
+def method_name():
+	from django.core.management import execute_from_command_line
+	return execute_from_command_line
+
 if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
         "DjangoWebProject1.settings"
     )
 
-    from django.core.management import execute_from_command_line
+    execute_from_command_line = method_name()
 
     execute_from_command_line(sys.argv)
